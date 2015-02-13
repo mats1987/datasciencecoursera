231bf05058e8b4a66427add4c9d5fc94d1d02670
Mathew Robert <mathew.robert123@gmail.com>
2015-02-13 18:49:57 +0530

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -7,8 +7,8 @@ pollutantmean <- function(directory,pollutant, id = 1:332) {
                 dat <- rbind(dat,read.csv(files_list[i]))
                 
         }
-        #dat_subset <- dat[which(dat[,"ID"] == id),]
-        dat_subset <- subset(dat,ID == id)
+        
+        dat_subset <- subset(dat,ID %in% id)
         mean(dat_subset[,pollutant],na.rm = T)
         
 }
